3ca95c94c547c8fd42c49a397eeeabcf865d2d3f
photofixyourstyle-coder <oldbutgoldth@gmail.com>
2026-07-16 00:56:13 +0700

    Repository

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { headers } from "next/headers";
 import "./globals.css";
 import "./portrait-lightbox.css";
+import "./admin-album-edit.css";
 
 export async function generateMetadata(): Promise<Metadata> {
   const requestHeaders = await headers();
