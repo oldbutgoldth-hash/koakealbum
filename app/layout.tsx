64a445023855917565cc2a3cee3703339a998cd0
photofixyourstyle-coder <oldbutgoldth@gmail.com>
2026-07-15 21:05:27 +0700

    แกล9

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { headers } from "next/headers";
 import "./globals.css";
-import "./lightbox-fix.css";
+import "./portrait-lightbox.css";
 
 export async function generateMetadata(): Promise<Metadata> {
   const requestHeaders = await headers();
